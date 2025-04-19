05784b606211db83490b1ddf57dabae7cf87d0a2
thursandi <thursandi@gmail.com>
2025-04-19 08:14:54 +0700

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.getElementById('btn-popup').addEventListener('click', function() {
+  document.getElementById('rekeningPopup').style.display = 'block';
+});
+
+function closePopup() {
+  document.getElementById('rekeningPopup').style.display = 'none';
+}
